9856a0523d205af39b880fb49c47ad05b07fbb21
tubofi <zhoupengtai@live.com>
2023-08-25 18:23:27 +0800

    2023.8.25

---
+++ src/components/try.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+    import { Group, Cell } from 'vux'
+    export default {
+      name: "try",
+      components: {
+        Group,
+        Cell
+      },
+      data(){
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
