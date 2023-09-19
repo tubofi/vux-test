8fcdf45b852aa6d2fe785682356ca53945cda975
tubofi <zhoupengtai@live.com>
2023-09-20 00:06:57 +0800

    2023.9.19

---
+++ src/components/Home-old.vue
@@ -0,0 +1,38 @@
+<template>
+  <group>
+    <x-header title="赛思少儿编程" :left-options="{showBack:false}"></x-header>
+
+    <div>
+      <img width="100%" src="../assets/main.png">
+    </div>
+    <flexbox orient="horizontal">
+      <flexbox-item span="50"><div></div></flexbox-item>
+      <flexbox-item span="50">
+        <div>
+        <x-button link="/sign" type="primary" style="font-size: 20px">👉点击报名👈</x-button>
+<!--          :gradients="['#2471ff', '#9733ff']"-->
+        </div>
+      </flexbox-item>
+      <flexbox-item span="50"><div></div></flexbox-item>
+
+    </flexbox>
+
+    <br><br><br>
+    <br><br><br>
+  </group>
+</template>
+
+<script>
+  import {Group, Cell, XHeader, XButton, XImg} from "vux"
+  export default {
+    name: "home",
+    components:{Group, Cell, XHeader, XButton, XImg},
+    data(){
+      return{}
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,40 @@
 <template>
   <group>
     <x-header title="赛思少儿编程" :left-options="{showBack:false}"></x-header>
-
+    <br>
     <div>
-      <img width="100%" src="../assets/main.png">
+      <img width="100%" src="../assets/header.png">
     </div>
-    <flexbox orient="horizontal">
-      <flexbox-item span="50"><div></div></flexbox-item>
-      <flexbox-item span="50">
-        <div>
-        <x-button link="/sign" type="primary" style="font-size: 20px">👉点击报名👈</x-button>
-<!--          :gradients="['#2471ff', '#9733ff']"-->
-        </div>
-      </flexbox-item>
-      <flexbox-item span="50"><div></div></flexbox-item>
-
-    </flexbox>
-
-    <br><br><br>
-    <br><br><br>
+    <div style="width: 80%;margin: 0 auto;">
+      <flexbox orient="vertical" :gutter="12">
+        <flexbox-item>1. 点击下方按钮填报学生信息（赛思少儿编程确保您的信息不会被泄露）</flexbox-item>
+        <flexbox-item><x-button link="/sign" type="primary" style="font-size: 20px">👉点击报名👈</x-button></flexbox-item>
+        <flexbox-item>2. 等待老师主动联系您确认学习相关事宜</flexbox-item>
+        <flexbox-item>3. 将指定文案连续在朋友圈转发三天</flexbox-item>
+        <flexbox-item>4. 参加scratch线上学习，三个月内免费</flexbox-item>
+        <flexbox-item>5. 试学后继续参加学习的同学按月缴费</flexbox-item>
+        <flexbox-item>
+          <img width="100%" src="../assets/present.png">
+        </flexbox-item>
+      </flexbox>
+    </div>
   </group>
 </template>
 
 <script>
-  import {Group, Cell, XHeader, XButton, XImg} from "vux"
+  import {Group, Cell, XHeader, XButton, XImg, Divider, Flexbox, FlexboxItem} from "vux"
   export default {
     name: "home",
-    components:{Group, Cell, XHeader, XButton, XImg},
+    components:{Group, Cell, XHeader, XButton, XImg, Divider, Flexbox, FlexboxItem},
     data(){
       return{}
     },
   }
 </script>
 
-<style scoped>
-
+<style lang="less">
+.btn_wrap {
+  padding: 0 1rem;
+  margin-top: 0rem;
+}
 </style>
